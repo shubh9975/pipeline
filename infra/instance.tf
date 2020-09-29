ecec156879924cdd5f1270be11fe6fa08e3a254c
root <root@shubh-virtual-machine>
2020-09-29 11:20:35 +0530

    fixes

--- infra/instance.tf
+++ infra/instance.tf
@@ -1,9 +1,3 @@
-provider "aws"{
-        access_key = "AKIARKOR3HZRZDZQ4M4M"
-	secret_key = "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"
-}
-   
-
 resource "aws_instance" "web" {
             ami= "var.ami_name"
             instance_type = "t2.micro"
