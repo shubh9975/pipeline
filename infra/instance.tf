ed2029d822dc90d828c1fc079f63bd2432b15985
root <root@shubh-virtual-machine>
2020-09-29 11:12:39 +0530

    fixes

--- infra/instance.tf
+++ infra/instance.tf
@@ -1,6 +1,6 @@
 provider "aws"{
-        profile = "test"
-        region = "ap-south-1"
+        access_key = "AKIARKOR3HZRZDZQ4M4M"
+	secret_key = "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"
 }
    
 
