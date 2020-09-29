d472780f9257386e54371e60aed56f01fd20fabc
root <root@shubh-virtual-machine>
2020-09-29 12:02:52 +0530

    update

--- infra/configurarion.tf
+++
@@ -1,6 +0,0 @@
-provider "aws"{
-        access_key = "AKIARKOR3HZRZDZQ4M4M"
-        secret_key = "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"
-        region = "ap-south-1"
-}
-
--- infra/instance.tf
+++ infra/instance.tf
@@ -1,3 +1,11 @@
+provider "aws"{
+        access_key = "AKIARKOR3HZRZDZQ4M4M"
+        secret_key = "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"
+        region = "ap-south-1"
+}
+
+
+
 resource "aws_instance" "web" {
   ami= "ami-09052aa9bc337c78d"
   instance_type = "t2.micro"
